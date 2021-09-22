fd0a5132949505be6c741e5ef71c05ea43da0ecd
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-22 20:42:36 +0000

    made changes to play.js

--- play.js
+++ play.js
@@ -2,3 +2,27 @@ const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', key => {
+    handleUserInput(key);
+  });
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  const stdout = process.stdout;
+  if (key === 'u\0003') {
+    stdout.write("Exited Snek Game, bye bye now. \n");
+    process.exit();
+  }
+}
+
+setupInput();
+handleUserInput();
