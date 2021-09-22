b725c950e370e6b713b090c57e486e07336ad813
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-22 21:14:42 +0000

    created constants.js

--- client.js
+++ client.js
@@ -1,30 +1,26 @@
-const net = require('net');
 
-const connect = function () {
+const net = require("net");
+const { IP, PORT } = require("./constants");
+const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
-    port: 50542// PORT number here,
+    host: IP ,// IP address here,
+    port: PORT // PORT number here,
   });
 
-  conn.setEncoding('utf8');
-
-  conn.on('connect', () => {
-    console.log("Connected to the SERVER!");
-    conn.write("Name: SAN");
-    conn.write("Say: What's good");
-    /*setInterval( func = () => {
-      conn.write("Move: up");
-    }, 500); */
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    // code that does something when the connection is first established
   });
+
+  conn.on("connect", () => {
+    conn.write("Name: JSP");
+    conn.write("Say: Hello Rob Ford");
+   
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
   return conn;
 };
-
-const conn = connect();
-
-conn.on('data', data => {
-  console.log(data);
-});
-
-
-module.exports = { connect };
+  
+module.exports = connect;
